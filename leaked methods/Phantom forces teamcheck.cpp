e3536b79c8ec09b5b361ca3d2b7f3f5844fceec2
azuraclient <121532542+azuraclient@users.noreply.github.com>
2026-04-17 06:54:27 +0800

    Create Phantom forces teamcheck.cpp

---
+++ leaked methods/Phantom forces teamcheck.cpp
@@ -0,0 +1,12 @@
+/// not cpp but let me tell you its easy
+
+/// you get all players team color then use your team color to check which is your teammates
+
+
+first get your own team color you need it -> get players -> get their teamcolor -> use your team color to differentiate teams
+
+// If you cant get your players use this from givenchy base
+
+storage::players = storage::datamodel.FindFirstChildOfClass("Players");  
+
+// hope this method dont get patched by Polye no need to credit
